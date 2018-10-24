38136a3d6299521d4d2f01f96a97ac0677809bd6
tracydaniellelong <tracydaniellelong@gmail.com>
2018-10-23 21:47:05 -0400

    test returns nil

--- hashmethods.rb
+++ hashmethods.rb
@@ -1,14 +1,14 @@
 
-def my_include?(value)
+def my_include?(num)
 	my_hash = {
 	1 => "a",
 	2 => "b"
 }
-	my_hash.each {|key| key <=> value}
-	if value != key
-		return false
-	else value = key
-		return true
+	my_hash.each{|key| key <=> num}
+	if key = num
+		puts "true"
+	else key != num
+		puts "false"
 	end
 
 end
