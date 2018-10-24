8fdbc7142713cdbf4949577d1d0cdbfa597c58c8
tracydaniellelong <tracydaniellelong@gmail.com>
2018-10-23 21:33:14 -0400

    something is missing

--- hashmethods.rb
+++ hashmethods.rb
@@ -1,8 +1,14 @@
 my_hash = {
-	1=>"a"
-	2=>"b"
+	1 => "a",
+	2 => "b"
 }
 
 def my_include?(value)
+	my_hash.each {|key| key <=> value}
+	if value != key
+		return false
+	else value = key
+		return true
+	end
 
 end
--- testhashmethods.rb
+++ testhashmethods.rb
@@ -1,6 +1,6 @@
 require 'minitest/autorun'
 require_relative 'hashmethods.rb'
 
-def test_include_a
-	assert_equal(true, my_include?(1))
+def test_a
+	assert_equal("true", my_include?(1))
 end
