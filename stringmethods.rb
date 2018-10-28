3a13cfeb1cd3e46aa2424262e2f22fada4271ee6
tracydaniellelong <tracydaniellelong@gmail.com>
2018-10-27 22:49:01 -0400

    test failed i expected a string instead of array

--- stringmethods.rb
+++ stringmethods.rb
@@ -1,6 +1,12 @@
 
-def my_sub!(str, oldtxt, newtxt)
-	my_string = str
-	my_string[oldtxt] = newtxt
-	return my_string
+# def my_sub!(str, oldtxt, newtxt)
+# 	my_string = str
+# 	my_string[oldtxt] = newtxt
+# 	return my_string
+# end
+
+def my_scan(str, txt)
+	matches = []
+	matches << str[txt] 
+	return matches
 end
--- teststringmethods.rb
+++ teststringmethods.rb
@@ -2,18 +2,18 @@ require 'minitest/autorun'
 require_relative 'stringmethods.rb'
 
 class Testing < Minitest::Test
-	def test_string_sub_e_to_3
-		str = "Hello World"
-		oldtxt = "e"
-		newtxt = "3"
-		assert_equal("H3llo World", my_sub!(str, oldtxt, newtxt))
-	end
-	def test_sub_6_to_5
-		str = "123466789"
-		oldtxt = "6"
-		newtxt = "5"
-		assert_equal("123456789", my_sub!(str, oldtxt, newtxt))
-	end
+	# def test_string_sub_e_to_3
+	# 	str = "Hello World"
+	# 	oldtxt = "e"
+	# 	newtxt = "3"
+	# 	assert_equal("H3llo World", my_sub!(str, oldtxt, newtxt))
+	# end
+	# def test_sub_6_to_5
+	# 	str = "123466789"
+	# 	oldtxt = "6"
+	# 	newtxt = "5"
+	# 	assert_equal("123456789", my_sub!(str, oldtxt, newtxt))
+	# end
 	def test_string_scan_for_h
 		str = "hello"
 		txt = "h"
