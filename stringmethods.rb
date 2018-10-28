be267b44aecef77edee9996520602321f5a24c6e
tracydaniellelong <tracydaniellelong@gmail.com>
2018-10-27 23:18:59 -0400

    another test

--- stringmethods.rb
+++ stringmethods.rb
@@ -1,9 +1,9 @@
 
-# def my_sub!(str, oldtxt, newtxt)
-# 	my_string = str
-# 	my_string[oldtxt] = newtxt
-# 	return my_string
-# end
+def my_sub!(str, oldtxt, newtxt)
+	my_string = str
+	my_string[oldtxt] = newtxt
+	return my_string
+end
 
 def my_scan(str, txt)
 	matches = []
--- teststringmethods.rb
+++ teststringmethods.rb
@@ -2,18 +2,18 @@ require 'minitest/autorun'
 require_relative 'stringmethods.rb'
 
 class Testing < Minitest::Test
-	# def test_string_sub_e_to_3
-	# 	str = "Hello World"
-	# 	oldtxt = "e"
-	# 	newtxt = "3"
-	# 	assert_equal("H3llo World", my_sub!(str, oldtxt, newtxt))
-	# end
-	# def test_sub_6_to_5
-	# 	str = "123466789"
-	# 	oldtxt = "6"
-	# 	newtxt = "5"
-	# 	assert_equal("123456789", my_sub!(str, oldtxt, newtxt))
-	# end
+	def test_string_sub_e_to_3
+		str = "Hello World"
+		oldtxt = "e"
+		newtxt = "3"
+		assert_equal("H3llo World", my_sub!(str, oldtxt, newtxt))
+	end
+	def test_sub_6_to_5
+		str = "123466789"
+		oldtxt = "6"
+		newtxt = "5"
+		assert_equal("123456789", my_sub!(str, oldtxt, newtxt))
+	end
 	def test_string_scan_for_h
 		str = "hello"
 		txt = "h"
@@ -23,5 +23,10 @@ class Testing < Minitest::Test
 		str = "hello"
 		txt = "l"
 		assert_equal(["l", "l"], my_scan(str, txt))
+	end
+	def test_scan_for_numbers
+		str = "2737475876754746"
+		txt = "7"
+		assert_equal(["7", "7", "7", "7", "7"], my_scan(str, txt))
 	end
 end
