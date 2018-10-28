85e4e505b68848ce3f6498a07378bc86ce8a8380
tracydaniellelong <tracydaniellelong@gmail.com>
2018-10-27 22:41:59 -0400

    made test for scan method

--- teststringmethods.rb
+++ teststringmethods.rb
@@ -8,10 +8,15 @@ class Testing < Minitest::Test
 		newtxt = "3"
 		assert_equal("H3llo World", my_sub!(str, oldtxt, newtxt))
 	end
-	def test_sub_
+	def test_sub_6_to_5
 		str = "123466789"
 		oldtxt = "6"
 		newtxt = "5"
 		assert_equal("123456789", my_sub!(str, oldtxt, newtxt))
+	end
+	def test_string_scan_for_h
+		str = "hello"
+		txt = "h"
+		assert_equal("h", my_scan(str, txt))
 	end
 end
