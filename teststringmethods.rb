f14a4262553538d5e3e31809492de23b021f4a0b
tracydaniellelong <tracydaniellelong@gmail.com>
2018-10-27 22:25:53 -0400

    made test pass

--- teststringmethods.rb
+++ teststringmethods.rb
@@ -9,9 +9,9 @@ class Testing < Minitest::Test
 		assert_equal("H3llo World", my_sub!(str, oldtxt, newtxt))
 	end
 	def test_sub_
-		str = "123455789"
-		oldtxt = "5"
-		newtxt = "6"
-		assert_equal("123466789", my_sub!(str, oldtxt, newtxt))
+		str = "123466789"
+		oldtxt = "6"
+		newtxt = "5"
+		assert_equal("123456789", my_sub!(str, oldtxt, newtxt))
 	end
 end
