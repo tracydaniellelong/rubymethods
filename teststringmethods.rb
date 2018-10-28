b6e091d8dc047801e6886a2e3ecb5fc2c0e9bb16
tracydaniellelong <tracydaniellelong@gmail.com>
2018-10-27 22:24:35 -0400

    my assertion was it would replace both 5's but it only replaced one

--- teststringmethods.rb
+++ teststringmethods.rb
@@ -2,10 +2,16 @@ require 'minitest/autorun'
 require_relative 'stringmethods.rb'
 
 class Testing < Minitest::Test
-	def test_string_sub
+	def test_string_sub_e_to_3
 		str = "Hello World"
 		oldtxt = "e"
 		newtxt = "3"
 		assert_equal("H3llo World", my_sub!(str, oldtxt, newtxt))
+	end
+	def test_sub_
+		str = "123455789"
+		oldtxt = "5"
+		newtxt = "6"
+		assert_equal("123466789", my_sub!(str, oldtxt, newtxt))
 	end
 end
