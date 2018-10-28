9d0a0d907130df448d47b819b0cc9f14c20430a1
tracydaniellelong <tracydaniellelong@gmail.com>
2018-10-27 23:52:27 -0400

    wrote a test for array that is empty, passed

--- testarraymethods.rb
+++ testarraymethods.rb
@@ -2,8 +2,12 @@ require 'minitest/autorun'
 require_relative 'arraymethods.rb'
 
 class Testing < Minitest::Test
-	def test_array_empty
+	def test_array_empty_false
 		ary = [1,2,3]
 		assert_equal(false, my_empty(ary))
+	end
+	def test_array_empty_true
+		ary = []
+		assert_equal(true, my_empty(ary))
 	end
 end
