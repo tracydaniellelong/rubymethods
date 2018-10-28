f86de1b39c59a104d3a6ed1b1870256dfc87f037
tracydaniellelong <tracydaniellelong@gmail.com>
2018-10-28 00:10:58 -0400

    wrote test for map method

--- testarraymethods.rb
+++ testarraymethods.rb
@@ -2,12 +2,17 @@ require 'minitest/autorun'
 require_relative 'arraymethods.rb'
 
 class Testing < Minitest::Test
-	def test_array_empty_false
-		ary = [1,2,3]
-		assert_equal(false, my_empty(ary))
+	# def test_array_empty_false
+	# 	ary = [1,2,3]
+	# 	assert_equal(false, my_empty(ary))
+	# end
+	# def test_array_empty_true
+	# 	ary = []
+	# 	assert_equal(true, my_empty(ary))
+	# end
+	def test_array_map_multiply_element_by_itself
+		ary = [1, 2, 3, 4]
+		assert_equal([1, 4, 9, 16], my_map(ary))
 	end
-	def test_array_empty_true
-		ary = []
-		assert_equal(true, my_empty(ary))
-	end
+
 end
