5958fb834968eb9b57b1f17cb0d65bb104a940d9
tracydaniellelong <tracydaniellelong@gmail.com>
2018-10-29 10:30:30 -0400

    made some more tests for sub method

--- teststringmethods.rb
+++ teststringmethods.rb
@@ -2,7 +2,8 @@ require 'minitest/autorun'
 require_relative 'stringmethods.rb'
 
 class Testing < Minitest::Test
-	def test_string_sub_e_to_3
+	# tests for sub method
+	def test_sub_e_to_3
 		str = "Hello World"
 		oldtxt = "e"
 		newtxt = "3"
@@ -14,19 +15,70 @@ class Testing < Minitest::Test
 		newtxt = "5"
 		assert_equal("123456789", my_sub!(str, oldtxt, newtxt))
 	end
-	def test_string_scan_for_h
-		str = "hello"
-		txt = "h"
-		assert_equal(["h"], my_scan(str, txt))
+	def test_sub_red_to_blue
+		str = "roses are red"
+		oldtxt = "red"
+		newtxt = "blue"
+		assert_equal("", my_sub(str, oldtxt, newtxt))
 	end
-	def test_string_scan_for_l
-		str = "hello"
-		txt = "l"
-		assert_equal(["l", "l"], my_scan(str, txt))
+	def test_sub_s_to_symbol
+		str = "wireless"
+		oldtxt = "s"
+		newtxt = "$"
+		assert_equal("", my_sub(str, oldtxt, newtxt))
 	end
-	def test_scan_for_numbers
-		str = "2737475876754746"
-		txt = "7"
-		assert_equal(["7", "7", "7", "7", "7", "7"], my_scan(str, txt))
+	def test_sub_t_to_7
+		str = "tracy"
+		oldtxt = "t"
+		newtxt = "7"
+		assert_equal("", my_sub(str, oldtxt, newtxt))
 	end
+	def test_sub_a_to_4
+		str = "hahahaha"
+		oldtxt = "a"
+		newtxt = "4"
+		assert_equal("", my_sub(str, oldtxt, newtxt))
+	end
+	def test_sub_o_to_symbol
+		str = "BOOHOO"
+		oldtxt = "o"
+		newtxt = "@"
+		assert_equal("", my_sub(str, oldtxt, newtxt))
+	end
+	def test_sub_p_l_and_e
+		str = "Happy Halloween"
+		oldtxt = "p", "l", "e"
+		newtxt = "6", "!", "9"
+		assert_equal("", my_sub(str, oldtxt, newtxt))
+	end
+	def test_sub_word
+		str = "A spooky witch"
+		oldtxt = "witch"
+		newtxt = "ghost"
+		assert_equal("", my_sub(str, oldtxt, newtxt))
+	end
+	def test_sub_sentence
+		str = "Head, shoulders, knees, and toes."
+		oldtxt = "e", "s"
+		newtxt = "3", "z"
+		assert_equal("", my_sub(str, oldtxt, newtxt))
+	end
+
+
+	# # tests for scan method
+	# def test_scan_for_letter_h
+	# 	str = "hello"
+	# 	txt = "h"
+	# 	assert_equal(["h"], my_scan(str, txt))
+	# end
+	# def test_scan_for_letter_l
+	# 	str = "hello"
+	# 	txt = "l"
+	# 	assert_equal(["l", "l"], my_scan(str, txt))
+	# end
+	# def test_scan_for_number_7
+	# 	str = "2737475876754746"
+	# 	txt = "7"
+	# 	assert_equal(["7", "7", "7", "7", "7", "7"], my_scan(str, txt))
+	# end
 end
