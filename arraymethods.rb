d6d224cfa5c0e59824b5b7e220089a04a7f24e69
tracydaniellelong <tracydaniellelong@gmail.com>
2018-10-29 14:58:56 -0400

    added operators for divide, add, and subtract

--- arraymethods.rb
+++ arraymethods.rb
@@ -1,10 +1,10 @@
-# def my_empty(ary)
-# 	if ary.size > 1
-# 		return false
-# 	else
-# 		return true
-# 	end
-# end
+def my_empty(ary)
+	if ary.size > 1
+		return false
+	else
+		return true
+	end
+end
 
 def my_map(ary, oper)
 	newary = []
--- testarraymethods.rb
+++ testarraymethods.rb
@@ -2,18 +2,44 @@ require 'minitest/autorun'
 require_relative 'arraymethods.rb'
 
 class Testing < Minitest::Test
-	# def test_array_empty_false
-	# 	ary = [1,2,3]
-	# 	assert_equal(false, my_empty(ary))
-	# end
-	# def test_array_empty_true
-	# 	ary = []
-	# 	assert_equal(true, my_empty(ary))
-	# end
-	def test_array_map_multiply_element_by_itself
+	# tests for empty method
+	def test_array_empty_false
+		ary = [1,2,3]
+		assert_equal(false, my_empty(ary))
+	end
+	def test_array_empty_true
+		ary = []
+		assert_equal(true, my_empty(ary))
+	end
+	def test_array_empty_string
+		ary = ["a", "b", "c", "d"]
+		assert_equal(false, my_empty(ary))
+	end
+	def test_array_empty_numbers
+		ary = ["1", "2", "3"]
+		assert_equal(false, my_empty(ary))
+	end
+
+	# tests for map method
+	def test_array_map_multiply
 		ary = [1, 2, 3, 4]
 		oper = "*"
 		assert_equal([1, 4, 9, 16], my_map(ary, oper))
+	end
+	def test_map_divide
+		ary = [1, 2, 3, 4]
+		oper = "/"
+		assert_equal([1, 1, 1, 1], my_map(ary, oper))
+	end
+	def test_map_add
+		ary = [1, 2, 3, 4]
+		oper = "+"
+		assert_equal([2, 4, 6, 8], my_map(ary, oper))
+	end
+	def test_map_subtract
+		ary = [1, 2, 3, 4]
+		oper = "-"
+		assert_equal([0, 0, 0, 0], my_map(ary, oper))
 	end
 
 end
